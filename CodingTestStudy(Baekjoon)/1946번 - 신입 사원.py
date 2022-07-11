17cd982af3be9fecec0da0658c193773e3d2c2f3
solbat <alswnd6397@gmail.com>
2022-07-12 00:21:38 +0900

    220711

---
+++ CodingTestStudy(Baekjoon)/1946번 - 신입 사원.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+testcases = []
+answer = []
+for i in range(t):
+    n = int(input())
+    answer.append([1]*n)
+
+    testcase = []
+    for j in range(n):
+        a, b = map(int, input().split())
+        testcase.append((a, b))
+    testcases.append(testcase)
+
+for i in range(t):
+    for candidate in testcases[i]:
+        for j in range(len(testcases[i])):
+            if candidate[0] < testcases[i][j][0] and candidate[1] < testcases[i][j][1]:
+                answer[i][j] = 0
+
+for i in range(t):
+    print(sum(answer[i]))
