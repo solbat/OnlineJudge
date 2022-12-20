2c25c91ac04349b0392e57bfcf4651dd3c2aad84
solbat <alswnd6397@gmail.com>
2022-12-21 01:31:00 +0900

    Revert "[solved] programmers lv1 제일 작은 수 제거하기"
    This reverts commit 3e9a059b63895bce284cf183cda183258b95a069.

--- Programmers/Swift/lv1/나누어 떨어지는 숫자 배열.swift
+++
@@ -1,9 +0,0 @@
-// Programmers
-// Swift
-// Lv. 1
-// 나누어 떨어지는 숫자 배열
-
-func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
-    let result = arr.filter({ $0 % divisor == 0 }).sorted()
-    return result.isEmpty ? [-1] : result
-}
